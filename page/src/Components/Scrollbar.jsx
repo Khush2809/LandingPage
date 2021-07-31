d8b22c651f30821f8024a7f6909c9837b1bcbe75
Khush2809 <2019khushboo.dhingra@ves.ac.in>
2021-07-31 23:53:33 +0000

    2 commit

--- page/src/Components/Scrollbar.jsx
+++ page/src/Components/Scrollbar.jsx
@@ -19,31 +19,134 @@ import img17 from "./../Images/img17.jpg";
 import img18 from "./../Images/img18.jpg";
 import img19 from "./../Images/img19.jpg";
 import "./../css/Scrollbar.css";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Scrollbar = () => {
+  var settings = {
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 100,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <marquee scrollamount="12" loop="infinite" scrolldelay="100">
-        <img src={img1} alt="img1" className="scroll"></img>
-        <img src={img2} alt="img2" className="scroll"></img>
-        <img src={img3} alt="img3" className="scroll"></img>
-        {/* <img src={img4} alt="img4" className="scroll"></img> */}
-        <img src={img5} alt="img5" className="scroll"></img>
-        <img src={img6} alt="img6" className="scroll"></img>
-        <img src={img7} alt="img7" className="scroll"></img>
-        <img src={img8} alt="img8" className="scroll"></img>
-        <img src={img9} alt="img9" className="scroll"></img>
-        <img src={img10} alt="img10" className="scroll"></img>
-        <img src={img11} alt="img11" className="scroll"></img>
-        <img src={img12} alt="img12" className="scroll"></img>
-        <img src={img13} alt="img13" className="scroll"></img>
-        <img src={img14} alt="img14" className="scroll"></img>
-        <img src={img15} alt="img15" className="scroll"></img>
-        <img src={img16} alt="img16" className="scroll"></img>
-        <img src={img17} alt="img17" className="scroll"></img>
-        <img src={img18} alt="img18" className="scroll"></img>
-        <img src={img19} alt="img19" className="scroll"></img>
-      </marquee>{" "}
+    // <div style={{ display: "flex", flexDirection: "row" }}>
+    //   <marquee scrollamount="12" loop="infinite" scrolldelay="100">
+    // <img src={img2} alt="img2" className="scroll"></img>
+    // <img src={img3} alt="img3" className="scroll"></img>
+    // {/* <img src={img4} alt="img4" className="scroll"></img> */}
+    // <img src={img5} alt="img5" className="scroll"></img>
+    // <img src={img6} alt="img6" className="scroll"></img>
+    // <img src={img7} alt="img7" className="scroll"></img>
+    // <img src={img8} alt="img8" className="scroll"></img>
+    // <img src={img9} alt="img9" className="scroll"></img>
+    // <img src={img10} alt="img10" className="scroll"></img>
+    // <img src={img11} alt="img11" className="scroll"></img>
+    // <img src={img12} alt="img12" className="scroll"></img>
+    // <img src={img13} alt="img13" className="scroll"></img>
+    // <img src={img14} alt="img14" className="scroll"></img>
+    // <img src={img15} alt="img15" className="scroll"></img>
+    // <img src={img16} alt="img16" className="scroll"></img>
+    // <img src={img17} alt="img17" className="scroll"></img>
+    // <img src={img18} alt="img18" className="scroll"></img>
+    // <img src={img19} alt="img19" className="scroll"></img>
+    //   </marquee>{" "}
+    // </div>
+    <div>
+      <Slider
+        {...settings}
+        //   style={{
+        //     display: "flex",
+        //     flexDirection: "row",
+        //     backgroundColor: "pink",
+        //   }}
+      >
+        <div>
+          <img src={img1} alt="img1" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img2} alt="img2" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img3} alt="img3" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img5} alt="img5" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img6} alt="img6" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img7} alt="img7" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img8} alt="img8" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img9} alt="img9" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img10} alt="img10" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img11} alt="img11" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img12} alt="img12" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img13} alt="img13" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img14} alt="img14" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img15} alt="img15" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img16} alt="img16" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img17} alt="img17" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img18} alt="img18" className="scroll"></img>
+        </div>
+        <div>
+          <img src={img19} alt="img19" className="scroll"></img>
+        </div>
+      </Slider>
     </div>
   );
 };
@@ -54,77 +157,77 @@ export default Scrollbar;
 
 // export default class Responsive extends Component {
 //   render() {
-//     var settings = {
-//       dots: true,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 4,
-//       slidesToScroll: 4,
-//       initialSlide: 0,
-//       responsive: [
-//         {
-//           breakpoint: 1024,
-//           settings: {
-//             slidesToShow: 3,
-//             slidesToScroll: 3,
-//             infinite: true,
-//             dots: true,
-//           },
-//         },
-//         {
-//           breakpoint: 600,
-//           settings: {
-//             slidesToShow: 2,
-//             slidesToScroll: 2,
-//             initialSlide: 2,
-//           },
-//         },
-//         {
-//           breakpoint: 480,
-//           settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1,
-//           },
-//         },
-//       ],
-//     };
+// var settings = {
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   initialSlide: 0,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true,
+//       },
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         initialSlide: 2,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// };
 //     return (
+//   <div>
+//     <h2> Responsive </h2>
+//     <Slider
+//       {...settings}
+//       style={{
+//         display: "flex",
+//         flexDirection: "row",
+//         backgroundColor: "pink",
+//       }}
+//     >
 //       <div>
-//         <h2> Responsive </h2>
-//         <Slider
-//           {...settings}
-//           style={{
-//             display: "flex",
-//             flexDirection: "row",
-//             backgroundColor: "pink",
-//           }}
-//         >
-//           <div>
-//             <h3>1</h3>
-//           </div>
-//           <div>
-//             <h3>2</h3>
-//           </div>
-//           <div>
-//             <h3>3</h3>
-//           </div>
-//           <div>
-//             <h3>4</h3>
-//           </div>
-//           <div>
-//             <h3>5</h3>
-//           </div>
-//           <div>
-//             <h3>6</h3>
-//           </div>
-//           <div>
-//             <h3>7</h3>
-//           </div>
-//           <div>
-//             <h3>8</h3>
-//           </div>
-//         </Slider>
+//         <h3>1</h3>
 //       </div>
+//       <div>
+//         <h3>2</h3>
+//       </div>
+//       <div>
+//         <h3>3</h3>
+//       </div>
+//       <div>
+//         <h3>4</h3>
+//       </div>
+//       <div>
+//         <h3>5</h3>
+//       </div>
+//       <div>
+//         <h3>6</h3>
+//       </div>
+//       <div>
+//         <h3>7</h3>
+//       </div>
+//       <div>
+//         <h3>8</h3>
+//       </div>
+//     </Slider>
+//   </div>
 //     );
 //   }
 // }
